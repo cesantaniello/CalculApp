c68a41ddfaa8654181a16a664642f011dace206e
Administrator <admin@example.com>
2021-08-24 16:40:52 +0200

    3.Add function sumar

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-console.log('app.js');
+function sumar() {
+    const forma = document.getElementById("forma");
+    const operandoA = forma["operandoA"];
+    const operandoB = forma["operandoB"];
+    let resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
+    if(isNaN(resultado))
+        resultado = "La operación no incluye números";
+    document.getElementById("resultado").innerHTML = `Resultado: ${resultado}`;
+    console.log(`Resultado: ${resultado}`);
+}
