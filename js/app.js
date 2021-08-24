1101addb03385d8e9339336731def09a7ec52966
Administrator <admin@example.com>
2021-08-24 16:47:55 +0200

    4.Add functions restar, multiplicar y dividir

--- js/app.js
+++ js/app.js
@@ -8,3 +8,38 @@ function sumar() {
     document.getElementById("resultado").innerHTML = `Resultado: ${resultado}`;
     console.log(`Resultado: ${resultado}`);
 }
+
+function restar() {
+    const forma = document.getElementById("forma");
+    const operandoA = forma["operandoA"];
+    const operandoB = forma["operandoB"];
+    let resultado = parseInt(operandoA.value) - parseInt(operandoB.value);
+    if(isNaN(resultado))
+        resultado = "La operación no incluye números";
+    document.getElementById("resultado").innerHTML = `Resultado: ${resultado}`;
+    console.log(`Resultado: ${resultado}`);
+}
+
+function multiplicar() {
+    const forma = document.getElementById("forma");
+    const operandoA = forma["operandoA"];
+    const operandoB = forma["operandoB"];
+    let resultado = parseInt(operandoA.value) * parseInt(operandoB.value);
+    if(isNaN(resultado))
+        resultado = "La operación no incluye números";
+    document.getElementById("resultado").innerHTML = `Resultado: ${resultado}`;
+    console.log(`Resultado: ${resultado}`);
+}
+
+function dividir() {
+    const forma = document.getElementById("forma");
+    const operandoA = forma["operandoA"];
+    const operandoB = forma["operandoB"];
+    let resultado = parseInt(operandoA.value) / parseInt(operandoB.value);
+    if(isNaN(resultado))
+        resultado = "La operación no incluye números";
+    if(operandoB.value == 0)
+        resultado = "La división por 0 no es posible";
+    document.getElementById("resultado").innerHTML = `Resultado: ${resultado}`;
+    console.log(`Resultado: ${resultado}`);
+}
